ecea266d0cddd6ff4aef444ee901dab6d7565aba
salt-dev <alexander.cheban@gmail.com>
2022-11-10 13:05:26 +0100

    added sass

--- client/src/components/artists-search/index.js
+++ client/src/components/artists-search/index.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from 'react';
 // import Card from '../card';
-// import './index.scss';
+import './index.scss';
 
 const Artists = ({ searchTerm, setSearchTerm }) => {
   const [similarArtists, setSimilarArtists] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [hasErrored, setHasErrored] = useState(false);
 
   useEffect(() => {
     const fetchArtists = async (search) => {
       try {
+        setIsLoading(true)
+        setTimeout(async() => {
         const response = await fetch(`http://localhost:8000/api/${search}`);
         const artist = await response.json()
         setSimilarArtists(artist);
         setSearchTerm('')
         setIsLoading(false)
+        }
+        , 2000);
       } catch (err) {
         setHasErrored(true);
         setIsLoading(false);
@@ -24,7 +28,7 @@ const Artists = ({ searchTerm, setSearchTerm }) => {
     if (searchTerm !== '') {
         fetchArtists(searchTerm);
     }
-  }, [searchTerm]);
+  }, [searchTerm, setSearchTerm]);
 
   if (similarArtists === 'WOOPS') {
     return <p>WOOPS, SOMETHING WENT WRONG</p>;
@@ -35,7 +39,16 @@ const Artists = ({ searchTerm, setSearchTerm }) => {
   }
 
   if (isLoading) {
-    return <p>...Loading</p>;
+    return <>
+    <div className="container">
+    <div className="everlib-logo">
+    <i className="everlib-logo-first-bar"></i>
+    <i className="everlib-logo-second-bar"></i>
+    <i className="everlib-logo-third-bar"></i>
+    <i className="everlib-logo-fourth-bar"></i>
+    </div>
+    </div>
+    </>;
   }
 
   return (
